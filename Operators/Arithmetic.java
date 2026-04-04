01f648ba5d612c584ba2ef916255925f1d579780
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2026-04-04 08:44:50 +0000

    Arithmetic Oeration

---
+++ Operators/Arithmetic.java
@@ -0,0 +1,9 @@
+package Operators;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        int A = 20;
+        int B = 10;
+        System.out.println("add :- " + (A+B));
+    }
+}
