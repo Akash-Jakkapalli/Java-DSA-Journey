5083696fb18150943278a49affb70bd3f4feed39
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2026-04-09 13:51:33 +0000

    basic commit

--- ConditionalStatement/loops.java
+++ ConditionalStatement/loops.java
@@ -52,16 +52,53 @@ public class loops {
 // -------------------------------------------------------------------------------------------
         // Largest number
 
-        int A = 2, B = 5, C = 10;
+        // int A = 2, B = 5, C = 10;
 
-        if ((A>=B) && (A>=C)) {
-            System.out.println("A");
-        }else if(B>=C) {
-            System.out.println("B");
-        }else{
-            System.out.println("C");
-        }
+        // if ((A>=B) && (A>=C)) {
+        //     System.out.println("A");
+        // }else if(B>=C) {
+        //     System.out.println("B");
+        // }else{
+        //     System.out.println("C");
+        // }
+// -----------------------------------Ternary  Operator--------------------------------------------------------
+        // Pass or Fail
 
+        // Scanner sc = new Scanner(System.in);
+        // int marks = sc.nextInt();
+
+        // String reportCard = (marks >= 30)? "PASS" : "FAIL";
+        // System.out.println(reportCard);
+        
+// -----------------------------------Swith Statement --------------------------------------------------------
+
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // char Operator = sc.next().charAt(0);
+
+        // switch (Operator) {
+        //     case '+':
+        //         System.out.println(a+b);
+        //         break;
+        //     case '-':
+        //         System.out.println(a-b);
+        //         break;
+        //     case '*':
+        //         System.out.println(a*b);
+        //         break;
+        //     case '/':
+        //         System.out.println(a/b);
+        //         break;
+        //     case '%':
+        //         System.out.println(a%b);
+        //         break;
+        //     default:
+        //         System.out.println("Wrong input");
+        //         break;
+        // }
+
+// -------------------------------------------------------------------------------------------
 
     }
 }
