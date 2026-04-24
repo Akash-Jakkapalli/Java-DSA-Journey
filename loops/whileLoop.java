d6d7852da456b3cef17cccee113374bbd0f9baa9
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2026-04-24 13:27:20 +0000

    while loop basic

---
+++ loops/whileLoop.java
@@ -0,0 +1,48 @@
+package loops;
+
+import java.util.Scanner;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        // int counter = 0;
+        // while (counter < 10) {
+        //     System.out.println("hello world");
+        //     counter++;
+        // }
+
+        //----------------print 1 to 10 numbers-------------------
+        // int counter = 1;
+        // while (counter <= 10) {
+        //     System.out.println(counter);
+        //     counter++;
+        // }
+        // System.out.println("1 to 10 numbers printed");
+
+        //----------------print 1 to N numbers-------------------        
+        // Scanner sc = new Scanner(System.in);
+        // int range = sc.nextInt();
+        // int counter = 1;
+
+        // while (counter <= range) {
+        //     System.out.print(counter + " ");
+        //     counter++;
+        // }
+        // System.out.println();
+
+        //----------------print 1 to N numbers-------------------     
+        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+
+        int i = 1;
+        while (i <= n) {
+            System.out.print(i + " ");
+            sum += i;
+            i++;
+        }
+        System.out.println("Sum is :- " + sum);
+        
+
+    }
+}
