e81dfee04d255761fef14f3d8af945629ee00474
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2026-04-25 04:13:05 +0000

    basic patterns printing

--- pattern1/pattern.java
+++ pattern1/pattern.java
@@ -23,14 +23,55 @@ public class pattern {
             * * 
             * 
         */
-        int n = 4;
-        for(int line = 1; line <= 4; line++){
+        // int n = 4;
+        // for(int line = 1; line <= 4; line++){
 
-            for(int star = 1; star <= 4-line+1; star++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        //     for(int star = 1; star <= 4-line+1; star++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        /*
+            1
+            12
+            123
+            1234
+        */
+        // int n = 4;
+        // for(int line = 1; line <= n; line++){
+        //     for(int num = 1; num <= line; num++){
+        //         System.out.print(num);
+        //     }
+        //     System.out.println();
+        // }
+
+        /*
+            A
+            BC
+            DEF
+            GHIJ
+        */
+        // int n = 4;
+        // char ch = 'A';
+
+        // // outer loop
+        // for(int line = 1; line <= n; line++){
+        //     //inner loop
+        //     for(int chars = 1; chars <= line; chars++){
+        //         System.out.print(ch);
+        //         ch++;
+        //     }
+        //     System.out.println();
+        // }
+
+        /*
+            * * * * *
+            *       *
+            *       *
+            * * * * *
+        */
+
         
     }
 }
