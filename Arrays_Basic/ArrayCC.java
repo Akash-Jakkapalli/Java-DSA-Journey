e26699ebcd2dd06f5c1ff3fc1935cc2f927985fa
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2026-05-05 07:06:53 +0000

    Creation of array

---
+++ Arrays_Basic/ArrayCC.java
@@ -0,0 +1,33 @@
+package Arrays_Basic;
+
+import java.util.Scanner;
+
+public class ArrayCC {
+    public static void main(String[] args) {
+
+        // Input
+        // Output
+        // Update
+
+        int marks[] = new int[10];
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("length of arry is " + marks.length);
+
+        // int math;
+        // math = sc.nextInt();
+
+        // marks[0] = sc.nextInt();
+        // marks[1] = sc.nextInt();
+        // marks[2] = sc.nextInt();
+
+        // System.out.println("math" + marks[0]);
+        // System.out.println("math" + marks[1]);
+        // System.out.println("math" + marks[2]);
+
+        // Update
+        marks[2] = 100;
+        // System.out.println("math" + marks[2]);
+
+    }
+}
