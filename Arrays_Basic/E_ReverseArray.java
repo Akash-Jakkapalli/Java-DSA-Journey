60cab2875738772c945bef8b9a21b2948b668193
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2026-05-05 13:53:48 +0000

    Arrays Part 1 is complited euuuuuu

---
+++ Arrays_Basic/E_ReverseArray.java
@@ -0,0 +1,33 @@
+package Arrays_Basic;
+
+public class E_ReverseArray {
+
+    public static void reverseArr(int arr[]){
+
+        int first = 0, last = arr.length-1;
+
+        while (first < last) {
+
+            // swap
+            int temp = arr[last];
+            arr[last] = arr[first];
+            arr[first] = temp;
+
+            first++;
+            last--;
+
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        int arr[] = {2, 4, 6, 8, 10};
+
+        reverseArr(arr);
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
