b8cb5074bbc23d9ba6b9d20eb29b7be1824bde2a
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2026-05-07 10:30:06 +0000

    buy and sell of stocks

---
+++ Arrays_Basic/J_BuySellStocks.java
@@ -0,0 +1,28 @@
+package Arrays_Basic;
+
+public class J_BuySellStocks {
+    
+    public static int buyAndSellStocks(int prices[]){
+
+        int buyingPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0; i<prices.length; i++){
+
+            if (buyingPrice < prices[i]) { // profit
+                int profit = prices[i] - buyingPrice; // this is todays profit 
+                maxProfit = Math.max(maxProfit, profit);
+            }else{
+                buyingPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        
+        int prices[] = {7, 1, 5, 3, 6, 4};
+
+        System.out.println(buyAndSellStocks(prices));
+    }
+}
