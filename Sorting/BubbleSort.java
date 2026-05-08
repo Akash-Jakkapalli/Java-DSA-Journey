25977166e34d164ba77b75a805d223334daac93a
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2026-05-08 17:48:50 +0000

    bubble sort file created

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,14 @@
+package Sorting;
+
+public class BubbleSort {
+
+    public static void bubbleSort(int arr[]){
+
+        
+    }
+
+    public static void main(String[] args) {
+        
+        int arr[] = {8, 5, 7, 2, 10, 3};
+    }
+}
