14374154f467d0c0dfc0f498bb5283e3d10119cc
Akash <145562031+Akash-Jakkapalli@users.noreply.github.com>
2026-05-09 08:15:05 +0000

    insertion sort

--- Sorting/Sorting.java
+++ Sorting/Sorting.java
@@ -48,11 +48,32 @@ public class Sorting {
         }
     }
 
+
+    // Insertion sort
+
+    public static void insertionSort(int arr[]){
+
+        for(int i=0; i<arr.length; i++){
+            
+            int curr = arr[i];
+            int prev = i-1;
+
+            //Finding out the correct possition to inser
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            // Insertion
+            arr[prev+1] = curr;
+        }
+    }
+
     public static void main(String[] args) {
         
         int arr[] = {5, 4, 1, 3, 2};
         // bubbleSort(arr);
-        selectionSort(arr);
+        // selectionSort(arr);
+        insertionSort(arr);
         printArr(arr);
 
     }
